7212c38d3865a22ba93b8a9287220361d3faaa15
Grzegorz s20198 <s20198@pjwstk.edu.pl>
2022-03-13 10:00:21 +0100

    next initial commiy

---
+++ Crawler/Crawler.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+
+namespace Crawler
+{
+    class Crawler
+    {
+        public async static Task Main(string[] args)
+        {
+            try
+            {
+                if (args.Length < 1)
+                    throw new ArgumentNullException();
+
+                var websiteUrl = args[0];
+
+                Regex regexUrl = new Regex(@"http[s]");
+
+                if (websiteUrl == "")
+                    throw new ArgumentException();
+
+                var httpClient = new HttpClient();
+
+                HttpResponseMessage response = await httpClient.GetAsync(websiteUrl);
+
+                if (!response.IsSuccessStatusCode)
+                    throw new Exception("Błąd podczas pobierania strony");
+
+                var content = await response.Content.ReadAsStringAsync();
+
+                Regex regex = new Regex(@"[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+");
+
+                MatchCollection matchCollection = regex.Matches(content);
+
+                var set = new HashSet<string>();
+
+                foreach (var item in matchCollection)
+                {
+                    set.Add(item.ToString());
+                }
+
+                foreach (var item in set)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+            catch (ArgumentNullException ane)
+            {
+                Console.WriteLine(ane.Message);
+            }
+            catch (ArgumentException ae)
+            {
+                Console.WriteLine(ae.Message);
+            }
+
+
+
+
+            //var list = new List<string>();
+            //var dictionary = new Dictionary<string, string>();
+            //var set = new HashSet<string>();
+
+            //            var a = $"Content {content}";
+            //            var b = "'";
+
+            //Console.WriteLine(matchCollection);
+
+            //Console.WriteLine(content);
+        }
+    }
+}
